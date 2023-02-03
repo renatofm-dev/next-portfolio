1ea2e94a126d6bba08ecf8ee84592563837e5715
Renato Martins <rmartins6694@gmail.com>
2023-02-03 18:06:24 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,17 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className='grid grid-cols-12 gap-6'>
+    <div>
+      sidebar
+    </div>
+    <div>
+        <Component {...pageProps} />
+
+    </div>
+
+  </div>
+  
+
+  
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,tsx}", "./components/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
