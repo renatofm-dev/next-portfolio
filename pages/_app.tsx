9891543160922d1d17470a0121b84b8192f493cc
Renato Martins <rmartins6694@gmail.com>
2023-02-06 13:39:12 -0300

    create a new component: navbar. create 2 new pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 
+import { Navbar } from '@/components/Navbar'
 import { Sidebar } from '@/components/Sidebar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
@@ -8,7 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className='col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl'>
       <Sidebar/>
     </div>
-    <div className='col-span-12 bg-white lg:col-span-9 rounded-2xl'>
+    <div className='flex flex-col col-span-12 bg-white lg:col-span-9 rounded-2xl '>
+      <Navbar />
       <Component {...pageProps} />
 
     </div>
---
+++ pages/projects.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const projects = () => {
+  return (
+    <div className='p-4'>
+        projects page
+    </div>
+  )
+}
