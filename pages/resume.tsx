3463b78b0054a99e816757c741bc965ad9d90054
Renato Martins <rmartins6694@gmail.com>
2023-02-06 14:46:25 -0300

    improve navbar

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const resume = () => {
+ const resume = () => {
   return (
     <div className='p-4 '>
         resume page
@@ -8,3 +8,5 @@ export const resume = () => {
   )
 }
 
+
+export default resume
