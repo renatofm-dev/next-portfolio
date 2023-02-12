694fda1ec8caa55b68769e35227f706563179f8b
Renato Martins <rmartins6694@gmail.com>
2023-02-12 12:25:32 -0300

    add new functions in projects.tsx

--- components/ProjectsNavbar.tsx
+++ components/ProjectsNavbar.tsx
@@ -3,18 +3,26 @@ import React, { FunctionComponent } from 'react'
 
 
 export const NavItem:FunctionComponent<{
-  value: Category
-}> = () => {
+  value: Category | "all"
+}> = ({value}) => {
   return (
-    <li>
-
+    <li className='capitalize cursor-pointer hover:text-green'>
+      {value}
     </li>
   )
 }
 
 const ProjectsNavbar = () => {
   return (
-    <div>ProjectsNavbar</div>
+    <div className='flex px-3 py-2 space-x-3 overflow-x-auto list-none'>
+      <NavItem value='all'/>
+      <NavItem value='React'/>
+      <NavItem value='React Native'/>
+      <NavItem value='Express'/>
+      <NavItem value='Node'/>
+      <NavItem value='Vanilla'/>
+      <NavItem value='Vue'/>
+    </div>
   )
 }
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,24 @@
 import ProjectCard from '@/components/ProjectCard'
-import { projects } from '@/data'
-import React from 'react'
+import ProjectsNavbar from '@/components/ProjectsNavbar'
+import { projects as projectsData } from '@/data'
+import { Category } from '@/types'
+import React, { useState } from 'react'
 
  const Projects = () => {
+  const [projects, setProjects] = useState(projectsData)
+
+  const handlerFillterCategory = (category: Category | "all") => {
+
+
+  }
+
+
+
+
   return (
     <div className='px-5 py-2 overflow-y-scroll' style={{height:'65vh'}}>
-       <nav>Navbar</nav>
+       
+       <ProjectsNavbar/>
        
 
       <div className='relative grid grid-cols-12 gap-4 my-3'>
