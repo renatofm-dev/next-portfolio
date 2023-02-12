4265545b65cabb4a28cdbf5181211d37eca6e33a
Renato Martins <rmartins6694@gmail.com>
2023-02-12 20:46:46 -0300

    create animation.ts and add animate in resume page and component bar

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,28 +1,41 @@
-import Bar from '@/components/Bar'
-import { languages, tools } from '@/data'
-import React from 'react'
+import Bar from '@/components/Bar';
+import { languages, tools } from '@/data';
+import React from 'react';
+import {motion} from 'framer-motion';
+import { fadeInUp } from '@/animations';
 
 const resume = () => {
+
+
+
   return (
     <div className='px-6 py-2'>
       {/* education and exp. */}
       <div className="grid gap-6 md:grid-cols-2">
-        <div>
+        <motion.div 
+          variants={fadeInUp}
+          initial="initial"
+          animate="animate"
+        >
           <h5 className='my-3 text-2xl font-bold '>Education</h5>
           <div>
             <h5 className='my-2 text-xl font-bold'>Chemical engineering student</h5>
             <p className='font-semibold'>lorem...</p>
             <p className='my-3'>lorem...</p>
           </div>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div 
+          variants={fadeInUp}
+          initial="initial"
+          animate="animate"
+        >
           <h5 className='my-3 text-2xl font-bold '>Experience</h5>
           <div>
             <h5 className='my-2 text-xl font-bold'>Frontend Developer Jr.</h5>
             <p className='font-semibold'>lorem...</p>
             <p className='my-3'>lorem...</p>
           </div>
-        </div>
+        </motion.div>
       </div>
       {/* Lenguages and tools */}
       <div className="grid gap-6 md:grid-cols-2">
