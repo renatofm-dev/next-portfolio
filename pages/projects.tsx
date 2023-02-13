9b0961055dd265c4f074ad21392c2ba3a6b9f66a
Renato Martins <rmartins6694@gmail.com>
2023-02-13 10:30:21 -0300

    add animations in page projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,8 @@ import ProjectsNavbar from '@/components/ProjectsNavbar'
 import { projects as projectsData } from '@/data'
 import { Category } from '@/types'
 import React, { useState } from 'react'
+import {motion} from 'framer-motion'
+import {stagger, fadeInUp} from '@/animations'
 
  const Projects = () => {
   const [projects, setProjects] = useState(projectsData)
@@ -22,27 +24,30 @@ import React, { useState } from 'react'
     setActive(category);
   };
 
-
-
-
   return (
     <div className='px-5 py-2 overflow-y-scroll' style={{height:'65vh'}}>
        
        <ProjectsNavbar handlerFillterCategory={handlerFillterCategory} active={active}/>
        
 
-      <div className='relative grid grid-cols-12 gap-4 my-3'>
+      <motion.div 
+        className='relative grid grid-cols-12 gap-4 my-3' 
+        variants={stagger} 
+        initial="initial" 
+        animate="animate"
+      >
         {
         projects.map(project => (
-          <div className='col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200'>
-            <ProjectCard
-              project={project}
-              key={project.name}
-            />
-          </div>
+          <motion.div 
+            className='col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200' 
+            variants={fadeInUp} 
+            key={project.name}
+          >
+            <ProjectCard project={project}/>
+          </motion.div>
         ))
         }
-      </div>
+      </motion.div>
 
     </div>
   )
