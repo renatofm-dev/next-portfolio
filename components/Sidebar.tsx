411ed88e88c4af1f38069a2ec4c3cf679e415c4e
Renato Martins <rmartins6694@gmail.com>
2023-03-28 13:30:45 -0300

    change buttons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -32,10 +32,10 @@ export const Sidebar = () => {
         <span className="text-redplanet-100">Renato </span>
          Martins
       </h3>
-      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200">Desenvolvedor Web & Gestor de Tráfego</p>
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-lg dark:bg-dark-200">Desenvolvedor Web & Gestor de Tráfego</p>
       <a
         href=""
-        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200"
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-lg dark:bg-dark-200"
         download='name'
       >
         <GiTie className="w-6 h-6" />
@@ -49,7 +49,7 @@ export const Sidebar = () => {
             aria-label="LinkedIn"
           />
         </a>
-        <a href="https://www.instagram.com/renatofm94/">
+        <a href="https://www.instagram.com/renatofm.dev/">
           <AiFillInstagram 
             className="w-8 h-8 cursor-pointer" 
             aria-label="Instagram"
@@ -63,7 +63,7 @@ export const Sidebar = () => {
         </a>
       </div>
       {/* address */}
-      <div className="py-4 my-5 bg-gray-200 dark:bg-dark-200" 
+      <div className="py-4 my-5 bg-gray-200 rounded-lg dark:bg-dark-200 " 
         style={{marginLeft: '-1rem', marginRight: '-1rem'}}>
         <div className="flex items-center justify-center space-x-2">
           <GoLocation />
@@ -74,19 +74,16 @@ export const Sidebar = () => {
         <a href="" className="hover:text-redplanet-100">
         <p className="my-2">+55 (61) 9 8197-5991</p>
         </a>
-        
-
-
       </div>
       {/* Email button */}
-      <button className="w-8/12 px-5 py-2 my-2 font-semibold text-white rounded-full bg-gradient-to-r from-redplanet-100 to-asiacolor-400 focus:outline-none"
+      <button className="w-8/12 px-5 py-2 my-2 font-semibold text-white rounded-lg bg-gradient-to-r from-redplanet-100 to-asiacolor-400 focus:outline-none"
         onClick={() => window.open('mailto: renatofm6694@gmail.com')}
       >
         Email
       </button>
       <button 
         onClick={changeTheme}
-        className="w-8/12 px-5 py-2 my-2 font-semibold text-white rounded-full bg-gradient-to-r from-redplanet-100 to-asiacolor-400">
+        className="w-8/12 px-5 py-2 my-2 font-semibold text-white rounded-lg bg-gradient-to-r from-redplanet-100 to-asiacolor-400">
         Theme
       </button>
     </div>
